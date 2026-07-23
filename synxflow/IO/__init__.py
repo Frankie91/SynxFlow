ecca83cd651fedae8e2c90bcb96d7095d6129034
Daniele Francario <44568289+Frankie91@users.noreply.github.com>
2026-07-23 15:28:45 +0200

    Update __init__.py

--- synxflow/IO/__init__.py
+++ synxflow/IO/__init__.py
@@ -9,10 +9,6 @@ Created on Wed Apr  1 14:56:15 2020
 @author: Xiaodong Ming
 """
 import numpy as np
-from .demo_functions import demo_input
-from .demo_functions import demo_output
-from .demo_functions import demo_raster
-from .demo_functions import get_sample_data
 from .InputModel import InputModel, load_input_object
 from .OutputModel import OutputModel, load_output_object
 from .indep_functions import clean_output, _dict2grid
